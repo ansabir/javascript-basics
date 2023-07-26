c7ef3419650ee6d1b4617865fb27e6c59954bb9a
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:35:48 +0530

    Learned about addition in javascript

---
+++ 06AddingNumber/script.js
@@ -0,0 +1,15 @@
+//Adding number
+
+//We can add two number with the (+) operator
+
+let a  = 10; //here we are declaring the value of 10 to the variable a
+console.log(a)
+let b = 20; //here wer are declaring the value of 20 tht the variable b
+console.log(b)
+let sum = a + b //here we are adding the value of a and b in the variable sum
+console.log(sum)
+
+
+//We can also do like below
+let total = 50 + 70;
+console.log(total)
