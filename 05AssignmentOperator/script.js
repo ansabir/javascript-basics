c8b2165daeb58f773dd0bf55fd0e49ea20f4f77d
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:28:15 +0530

    Learned about the assignment operator

---
+++ 05AssignmentOperator/script.js
@@ -0,0 +1,13 @@
+// Assingment Operator ( = )
+
+let a; //here we declare wthe variable a
+console.log(a) //we will get null
+
+a = 10; //here we assin value of 10 to the variable a
+console.log(a) //we will get 10
+
+let b = 20  //here we directly inializing the variable with the value of 20
+console.log(b)
+
+b = a; //here we are telling that the value of b is eqaul to a so now, we will get the 10
+console.log(a)
