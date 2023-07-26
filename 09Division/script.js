2627032b5565c21ceba2bb83f58cdc6c0a2bf04a
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:54:09 +0530

    Learned about division in js

---
+++ 09Division/script.js
@@ -0,0 +1,12 @@
+// Division (/)
+
+
+let a  = 50 //here we are declaring the variable with the value 50
+console.log("Value of a", a)
+
+let b = 5 //here we are declaring the variable with the value of 5
+console.log("Value of b", b)
+
+let div = a / b //here we are dividing variable a with variable b
+console.log("Result after dividing a with b", div)
+
