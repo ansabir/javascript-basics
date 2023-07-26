029935be531a7f3f8ec5e9bce979d0589c6d8c00
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:55:05 +0530

    Renamed the folder

---
+++ 08Multiplication/script.js
@@ -0,0 +1,9 @@
+//Multiplication
+let a = 5; //here were are initialing the variable with the value of 5
+console.log("Value of a", a)
+
+let b = 8; //here were are initialing the variable with the value of 8
+console.log("Value of b", b)
+
+let multi = a * b //here were mulitplying variable a with variable b
+console.log("Result of 5 * 8 is eqaul to", multi)
