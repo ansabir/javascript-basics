ec6d8f75aa6995731de65f1d1da376b8ff036672
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:40:39 +0530

    Learned how to do substraction in JS

---
+++ 07Substraction/script.js
@@ -0,0 +1,15 @@
+//Substraction
+
+//We can add two number with the (+) operator
+
+let a  = 30; //here we are declaring the value of 10 to the variable a
+console.log(a)
+let b = 20; //here wer are declaring the value of 20 tht the variable b
+console.log(b)
+let sum = a - b //here we are substracting the value of a and b in the variable sum
+console.log(sum)
+
+
+//We can also do like below
+let total = 70 - 20;
+console.log(total)
