7f4c9ff2afbfbafda3ef1a88923ce9c2496e0856
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:10:04 +0530

    Learned about javascript datatypes

---
+++ 03DataTypes/script.js
@@ -0,0 +1,24 @@
+/*Data Types:
+undefined, null, objects, boolean, symbol, number and object
+*/
+
+//undefined
+//undefined is something that has not been defined. For example after creating the variable we have not defined any value to it
+
+//null
+//null means nothing!
+
+//boolean
+//boolean is true or false
+
+//string
+//a set of charaters wihtin the single or double inverted coma
+
+//symbol
+//a symbol is an immutable premitive value that is unique
+
+//number
+//a number is a number
+
+//object
+//an object can store different key value pairs of data
