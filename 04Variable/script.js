a0d1c1f1bffcde505c574e4c91c7fee6b063d02a
Sabir Ansari <ansabir95@gmail.com>
2023-07-26 20:16:29 +0530

    Learned about variables

---
+++ 04Variable/script.js
@@ -0,0 +1,13 @@
+//Variable
+//Variable is like a container in which we store the value
+//We can set variable with var, let, or conts keywork
+
+//Example
+var my_name = "Sabir" //her variable name "my_name" holds the value of string
+console.log("My name is ", my_name)
+
+let my_age = 28
+console.log("I am ", my_age, "years old")
+
+const degree ="BSc(Computer science)"
+console.log("I have done", degree)
